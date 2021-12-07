8d1f9f98f034318e59ae9194fdae434dceab9f51
Andrés Marín <felipemarin0152@gmail.com>
2021-12-06 19:06:56 -0500

    Cambios por el logo

--- src/Components/03-multipleHooks/Multipleshooks.jsx
+++ src/Components/03-multipleHooks/Multipleshooks.jsx
@@ -24,7 +24,7 @@ const Multipleshooks = () => {
             <header className="center ">
               <img
                 src={
-                  imagen
+                  "https://raw.githubusercontent.com/smilegupta/breaking-bad/master/src/img/logo.png"
                 }
                 alt=""
               />
